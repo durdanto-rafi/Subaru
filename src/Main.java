d12df2fda4beda460760a274b4cd7ce281d4ce22
Golam Mahmud Rafi <rafi.aust@live.com>
2017-07-11 18:41:07 +0900

    Graph Added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Main{
 	public static void main(String[] args)
 	{
-		Activity activity = new Activity();
-		List<Event> events = activity.getData();
-		System.out.println(events.size());
+		
 	}
 }
